1ef223e5f8f32d45bdb309569b48dcfa85cd68b2
David <davidaicardocossiopulido@gmail.com>
2021-04-21 16:51:07 +0200

    EMPECE A HACER EL MENU

--- trabajoElena1/src/trabajoElena1/matriz.java
+++ trabajoElena1/src/trabajoElena1/matriz.java
@@ -4,7 +4,26 @@ public class matriz {
 
 	public static void main(String[] args) {
 		
-
+		Scanner sn = new Scanner(System.in);
+		
+		boolean salir = false;
+		
+		while(!salir) {
+			
+			System.out.println("1. Suma de dos matrices ");
+			System.out.println("2. Producto de un escalar por una matriz");
+			System.out.println("3. Producto de dos matrices");
+			System.out.println("4. Transponer una matriz");
+			System.out.println("5. Diiagonal principal de una matriz");
+			System.out.println("6. Comprobar si una matriz es simetrica");
+			System.out.println("7. Potencia de una matriz cuadrada");
+			System.out.println("8. Resta de dos matrices");
+			System.out.println("SALIR");
+			System.out.println("");
+			
+			
+		}
+		
 	}
 
 }
